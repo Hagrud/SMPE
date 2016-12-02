a3829c2156a7f01e8286e8d04d9af85b333e817b
pep <pep.polet@gmail.com>
2016-12-02 06:42:48 +0100

    analyse

--- smpe.r
+++ smpe.r
@@ -33,8 +33,6 @@ We need more data to make the conclusion. So we do some new experiments, and her
 
 ```{r}
 data_1 = read.csv("archive_quicksort/measurements_computer1.csv")
-#data_2 = read.csv("archive_quicksort/measurements_computer2.csv")
-#data_3 = read.csv("archive_quicksort/measurements_computer3.csv")
 
 ggplot(data_1, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
 ```
@@ -43,24 +41,57 @@ On this experiment, we see tat the parallel algorithm have a bigger variance tha
 
 
 ```{r}
-dataSeq = data_1[data$Type==" Sequential",]
-reg <- lm(data = dataSeq, dataSeq$Time~I(dataSeq$Size*log(dataSeq$Size)))
+dataSeq_1 = data_1[data$Type==" Sequential",]
+reg <- lm(data = dataSeq_1, dataSeq_1$Time~I(dataSeq_1$Size*log(dataSeq_1$Size)))
 summary(reg)
 ```
 
 ```{r}
 
-graph = ggplot(dataSeq, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+graph = ggplot(dataSeq_1, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
 
 graph + geom_smooth(method = "lm", formula = y ~ I(x*log(x))) +  geom_smooth(method = "lm", formula = y ~ x, color="black") 
 ```
 
 ```{r}
+dataBIn_1 = data_1[data$Type==" Built-in",]
+reg <- lm(data = dataBIn_1, dataBIn_1$Time~I(dataBIn_1$Size*log(dataBIn_1$Size)))
+summary(reg)
+graph = ggplot(dataBIn_1, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+
+graph + geom_smooth(method = "lm", formula = y ~ I(x*log(x)))
+```
+The built-in method give almost the same results that the sequential one, we can assume they can be implemented the same way?
+
+
+```{r}
+dataPar_1 = data_1[data$Type==" Parallel",]
+reg <- lm(data = dataPar_1, dataPar_1$Time~I(dataPar_1$Size*log(dataPar_1$Size)))
+summary(reg)
+graph = ggplot(dataPar_1, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+
+graph + geom_smooth(method = "lm", formula = y ~ I(x*log(x)))
 ```
 
 
-#Furthermore, 
+We see that the parallel aglorithm have a totally different result between the two dataset, we decide to make another experiments with others computers.
 
+
+```{r}
+data_2 = read.csv("archive_quicksort/measurements_computer2.csv")
+data_3 = read.csv("archive_quicksort/measurements_computer3.csv")
+
+ggplot(data, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+ggplot(data_1, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+ggplot(data_2, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+ggplot(data_3, aes(x=Size, y=Time, color = factor(Type))) + geom_point()
+```
+Two computer on our workbench seems to not take advantage of the parallelism, maybe because of their architecture.
+
+```{r}
+```
+```{r}
+```
 
 
 
